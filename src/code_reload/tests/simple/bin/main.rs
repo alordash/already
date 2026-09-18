8e7e312d8a69ff63e3daf33aa842ec08743a62c4
alordash <alordash@mail.ru>
2026-09-19 00:09:47 +0400

    flacky test fix

--- src/code_reload/tests/simple/bin/main.rs
+++ src/code_reload/tests/simple/bin/main.rs
@@ -6,6 +6,7 @@ fn main() -> Result<(), std::io::Error> {
 
     println!();
     let _sync = std::io::stdin().read_line(&mut String::new())?;
+    std::thread::sleep(std::time::Duration::from_secs(1)); // wait until OS sends an event
 
     let after_hotreload = get();
     assert_eq!(after_hotreload, 2);
