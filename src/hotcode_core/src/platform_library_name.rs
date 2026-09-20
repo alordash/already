ab3511a1205cb9d264ce2374938f90d069ae787d
alordash <alordash@mail.ru>
2026-09-21 00:31:15 +0400

    wip

--- src/hotcode_core/src/platform_library_name.rs
+++ src/hotcode_core/src/platform_library_name.rs
@@ -5,6 +5,10 @@ pub fn get_platform_library_file_name(library_name: &str) -> String {
     {
         return format!("{library_name}.dll");
     }
+    #[cfg(target_os = "macos")]
+    {
+        return format!("lib{library_name}.dylib");
+    }
     #[cfg(unix)]
     {
         return format!("lib{library_name}.so");
