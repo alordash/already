6b64b5af5cd24073bc12aa21699f5b8c8ec48f27
alordash <alordash1@gmail.com>
2026-09-21 14:18:22 +0400

    more debug

--- src/hotcode/tests/lib_bin_together/bin/bin_together.rs
+++ src/hotcode/tests/lib_bin_together/bin/bin_together.rs
@@ -13,15 +13,20 @@ fn main() -> Result<(), std::io::Error> {
     )));
 
     println!();
-    dbg!(old_library.strong_count());
+    dbg!(old_library.strong_count(), old_library.as_ptr());
     let _sync = std::io::stdin().read_line(&mut String::new())?;
 
     // Wait for old library to be updated and unloaded
-    dbg!(old_library.strong_count());
+    dbg!(old_library.strong_count(), old_library.as_ptr());
     while old_library.strong_count() > 0 {
         std::hint::spin_loop();
     }
-    dbg!(old_library.strong_count());
+    dbg!(old_library.strong_count(), old_library.as_ptr());
+
+    let new_library = Arc::downgrade(&provide_library_wrapper(&get_platform_library_file_name(
+        "lib_bin_together",
+    )));
+    dbg!(new_library.strong_count(), new_library.as_ptr());
 
     let new_values = get_values();
     let expected_new_values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110];
